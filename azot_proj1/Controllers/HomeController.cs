c79a2371ea3a15561ff0372d7af37929352538bb
Vlad <lemon444eg@gmail.com>
2019-08-12 20:24:20 +0700

    first commit

---
+++ azot_proj1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using azot_proj1.Models;
+using azot_proj1;
+
+namespace azot_proj1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public azot_db1Entities db;
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("indexView",db);
+        }
+
+        public HomeController() {
+             db = new azot_db1Entities();
+            
+        }
+    }
+}
